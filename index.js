f1a4b28b1326b1066865aa047b5b6e8705c888de
Anpr1211 <ankita.prakash1998@gmail.com>
2024-10-08 15:52:33 +0530

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { resolve } = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = 3010;
+
+app.use(cors());
+
 
 let taxRate = 5;
 let discountPercentage = 10;
